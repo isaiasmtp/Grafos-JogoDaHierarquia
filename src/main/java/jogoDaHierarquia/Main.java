d5e707e24ef0eb7f024402ebf33a14698c19877a
Isaías Martins Teixeira Pontes <isaias.pontes@ccc.ufcg.edu.br>
2019-05-09 09:21:36 -0300

    Finalizando Main

--- src/main/java/jogoDaHierarquia/Main.java
+++ src/main/java/jogoDaHierarquia/Main.java
@@ -2,7 +2,9 @@ package jogoDaHierarquia;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Set;
 
 import org.jgrapht.Graph;
@@ -26,8 +28,10 @@ public class Main {
 		
 		Graph<String, DefaultEdge> graph = new SimpleGraph<>(DefaultEdge.class);
 		
-		 Graph<String, DefaultEdge> directedGraph =
+		Graph<String, DefaultEdge> directedGraph =
 	                new DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
+		
+		Scanner scanner = new Scanner(System.in);
 
 		graph = MyJGraphTUtil.importGraphCSV(
 				graph, 
@@ -36,41 +40,45 @@ public class Main {
 				false,
 				false,
 				true); // MATRIX_FORMAT_NODEID
-			
-		//Quantidade de tentativas - DEVE SER SETADO 
-		int tentativas = 4;
-         
+			         
         //Cria um ArrayList de Vertices
         ArrayList<String> vertices = new ArrayList<String>(graph.vertexSet());
-
-
+        
         //Seleciona um vertice aleatorio para ser a raiz
         Random r = new Random();
         String raiz = vertices.get(r.nextInt(vertices.size()));
-        
-        //Vertice 8 mocado para testes 
-        //TODO
-        raiz = "8";
-
+        raiz = "8";//TODO
        
-       
-		MyTreeUtil.getRootedTree(((SimpleGraph<String, DefaultEdge>) graph), "7",((DefaultDirectedGraph<String, DefaultEdge>) directedGraph));
-        
-    	 MyJGraphTUtil.createAndShowGui(directedGraph,"Graph",
+		MyTreeUtil.getRootedTree(((SimpleGraph<String, DefaultEdge>) graph), raiz,((DefaultDirectedGraph<String, DefaultEdge>) directedGraph));    	 
+    	 
+    	 Set<DefaultEdge> E = new HashSet<>(directedGraph.edgeSet());
+    	 
+		 System.out.print("Quantidade de tentativas para o jogador: ");
+		 
+		 int tentativas = Integer.parseInt(scanner.next());
+		 
+		 MyJGraphTUtil.createAndShowGui(directedGraph,"Graph",
                  false,false,true,true,
                  MyJGraphTUtil.layout_type.CIRCLE);
+	    System.out.println("Arvore: "+ graph.edgeSet()); 
 
-        
-//    	System.out.println("Vertices: " + directedGraph.edgeSet());        
-//		System.out.println("Vertices: " + graph.edgeSet());
-		System.out.println("Arestas: " + directedGraph.edgeSet());
-	}
-	
-	public void pais() {
-		
-	}
-
-	public void filhos() {
-		
+    	 while(tentativas != 0) {
+    		 
+    		 System.out.print("Indique o vertice: ");
+    	     String v = scanner.next();
+    	     if(v.equals(raiz)) {
+    	    	System.out.println("Você acertou! "); 
+    	    	System.out.println("Arvore Enraizada: "+ directedGraph.edgeSet()); 
+    	    	break;
+    	     }
+    	     System.out.println(v +" não é raiz. O pai de "+ v + " é " + util.Util.pais(E, v) +
+    	    		 " e os filhos de " + v + " são " + util.Util.filhos(E, v) +".");
+    	     tentativas--;
+    	     
+    	     if(tentativas==0) {
+    	    	System.out.println("Número de tentativas excedido!");
+     	    	System.out.println("Arvore Enraizada: "+ directedGraph.edgeSet()); 
+    	     }         	     		 
+    	 }   	 
 	}
 }
